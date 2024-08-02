2f63869327157d04e48c473c5c1e7aeb596f28f3
Moorpho <grib.sofia.ua@gmail.com>
2024-08-02 16:05:25 +0100

    FirstElevenLessons

---
+++ script.js
@@ -0,0 +1,49 @@
+a = 5;
+console.log(a);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+
+let person = {
+    name1: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name1);
+
+
+let arr = ['apple','orange','pear']
+
+console.log(arr[0])
+ 
+//let answer = prompt('Чи є вам 18?','Да');
+//console.log(typeof(null));
+
+//console.log("")
+
+
+
+//alert('Hello world!')
+
+//let answer = confirm('Are you here?')
+
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr-- );
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClosed = true;
+
+console.log(isChecked || !isClosed);
+
+
---
+++ script4.js
@@ -0,0 +1,28 @@
+function first(
+    setTimeout(() => {
+        
+    }, timeout);
+)
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang,callback) {
+    console.log("I learn"+ lang);
+    callback();
+}
+function done() {
+    console.log("I did 3rd lesson!")
+}
+learnJS("JavaScript", done);
+
+()=>{}
+function(){}
+function name(){}
+setTimeout(
+
+)
