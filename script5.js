5b82610fc7dfb7410bc89e26a2039860255ebf01
Moorpho <grib.sofia.ua@gmail.com>
2024-08-05 16:21:09 +0100

    different methods with arrays

--- script5.js
+++ script5.js
@@ -1,39 +1,47 @@
-let options = {
-    width: 1024,
-    height: 1024,
-    name:"test",
-};
+// let options = {
+//     width: 1024,
+//     height: 1024,
+//     name:"test",
+// };
 
-console.log(options.name);
-options.bool = false;
-options.colors = {
-    border: "black",
-    bg: "red"
-};
-delete options.bool;
+// console.log(options.name);
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     bg: "red"
+// };
+// delete options.bool;
 
-for (let key in options) {
-    console.log('Parameter ' + key  + ' have a value of ' + options[key] );
-}
+// for (let key in options) {
+//     console.log('Parameter ' + key  + ' have a value of ' + options[key] );
+// }
 
-console.log(Object.keys(options).length)
+// console.log(Object.keys(options).length)
 
-let arr = ["first",2,3,"four",5];
+// let arr = ["first",2,3,"four",5];
 
-console.log(arr);
+// console.log(arr);
 
 // for (let i = 0; i < arr.length;i++) {
 //     console.log(arr[i]);
 // }
 
-arr.forEach(function(item, i, mass){
-    console.log(i + ':' + item + "(array: " + mass + ")")
-})
+// let ans = prompt("",""),
+// arr = [];
+
+// arr = ans.split(',');
+// console.log(arr);
+
+// let arr = ["sdsd","sdere","dd","ewr"],
+// i =arr.join(', ');
+
+// console.log(i);
+
+let arr = [1,15,4],
+i = arr.sort(compareNum);
+
+function compareNum(a,b){
+    return a-b;
+}
 
 console.log(arr);
-
-let mass = [1,3,4,6,7];
-
- for (let key of mass) {
-    console.log(key);
- }
