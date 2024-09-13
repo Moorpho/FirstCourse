c57a6c84db9aabed05ddb513c4d7d6ae5556ccf3
Moorpho <grib.sofia.ua@gmail.com>
2024-09-13 22:17:09 +0100

    functions

--- script7.js
+++ script7.js
@@ -1,13 +1,26 @@
 
-var calculateDistance = function(time, speedOfFirstCyclist,speedOfSecondCyclist){
-    var distanceOfFirstCyclist = speedOfFirstCyclist * time;
-    var distanceOfSecondCyclist = speedOfSecondCyclist * time;
+// var calculateDistance = function(time, speedOfFirstCyclist,speedOfSecondCyclist){
+//     var distanceOfFirstCyclist = speedOfFirstCyclist * time;
+//     var distanceOfSecondCyclist = speedOfSecondCyclist * time;
     
-    var totalDistance = distanceOfFirstCyclist + distanceOfSecondCyclist;
+//     var totalDistance = distanceOfFirstCyclist + distanceOfSecondCyclist;
     
-    return totalDistance;
-};
+//     return totalDistance;
+// };
+// var measureUnit = 'км';
+// console.log('Distance between first cities: ' + calculateDistance(3,12,14) + measureUnit);
+// console.log('Distance between second cities: ' + calculateDistance(5,1,2) + measureUnit);
 
-var firstDistance = calculateDistance(3,12,14);
-var secondDistance = calculateDistance(5,1,2);
-console.log(firstDistance,secondDistance);
+// var secondDistance = calculateDistance(5,1,2);
+// console.log(firstDistance,secondDistance);
+
+var buySomeBread  = function(eggs){
+//  if (eggs ) {
+//     return 10;
+//  } else {
+//     return 1;
+//  }
+
+return eggs ? 10 : 1;
+};
+console.log(buySomeBread(false));
